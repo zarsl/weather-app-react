a62672ecd767435548bc5b92eed378975fc40189
zarsl <64368356+zarsl@users.noreply.github.com>
2020-12-02 14:31:24 -0500

    Added bootstrap and updated file to match initial design

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather city="Paris" />
+        <Weather />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,127 @@
-import Loader from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
-  //   function handleResponse(response) {
-  //     alert(`The weather in ${props.city} is ${response.data.main.temp}`);
-  //   }
-
-  //   let apiKey = "4fb8f394cc5f2d439df6249cf258d6a4";
-  //   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  //   axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    localTime: "00:00",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    temperature: "60",
+    humidy: "10",
+    wind: "1",
+  };
   return (
-    <Loader
-      type="Puff"
-      color="#00BFFF"
-      height={100}
-      width={100}
-      timeout={5000} //3 secs
-    />
+    <div className="Weather">
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app">
+            <form id="search">
+              <div className="row">
+                <div className="col-8" id="input-field">
+                  <input
+                    type="search"
+                    className="form-control"
+                    placeholder="Type a city..."
+                    autofocus="on"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="form-control btn btn-primary"
+                  />
+                </div>
+                <div className="col-1">
+                  <input
+                    type="submit"
+                    value="📌"
+                    className="form-control btn btn-outline-secondary"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title="Search by current location"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="upper-section mx-auto">
+              <h1>{weatherData.city}</h1>
+              <ul>
+                <li>
+                  Local Time: <span>{weatherData.localTime}</span>
+                </li>
+                <li></li>
+              </ul>
+              <div className="row">
+                <div className="col-7">
+                  <div className="clearfix">
+                    <span>
+                      <img
+                        className="weather-icon float-left"
+                        src={weatherData.imgUrl}
+                      />
+                    </span>
+                    <div className="float-left">
+                      <span className="temperature">
+                        {weatherData.temperature}
+                      </span>
+                      <span className="units">
+                        <a href="/" className="active">
+                          °F
+                        </a>{" "}
+                        |<a href="/">°C</a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-5">
+                  <ul>
+                    <li>
+                      Humidity: <span>{weatherData.humidy}</span>%
+                    </li>
+                    <li>
+                      Wind: <span>{weatherData.wind}</span>
+                      <span>mi/hr</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="hourly-forecast">
+              <div className="row weather-forecast"></div>
+            </div>
+            <p>Weekly Forecast</p>
+            <div className="weekly-forecast mx-auto">
+              <div className="row weather-forecast"></div>
+            </div>
+            <div id="footer">
+              <div className="row">
+                <div className="col-6 hour-toggle">
+                  <a href="#" className="active">
+                    12 hr
+                  </a>{" "}
+                  |<a href="#">24 hr</a>
+                </div>
+                <div className="col-6 last-update">
+                  <em>
+                    {" "}
+                    Last Updated: <span>18:00</span>
+                  </em>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer>
+            <a
+              href="https://github.com/zarsl/vanilla-weather-app"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            by Zeynep Arslan 👩‍💻
+          </footer>
+        </div>
+      </div>
+    </div>
   );
 }
